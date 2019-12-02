ba39b5724e893ea4f3937805f1a6da81fe64d2c6
KillerCode_236 <45013989+Rubencrash236@users.noreply.github.com>
2019-12-02 00:16:58 -0500

    n

--- Consultorio/config/database.php
+++ Consultorio/config/database.php
@@ -83,7 +83,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'test'),
+            'database' => env('DB_DATABASE', 'Hospital'),
             'username' => env('DB_USERNAME', 'sa'),
             'password' => env('DB_PASSWORD', '1234'),
             'charset' => 'utf8',
